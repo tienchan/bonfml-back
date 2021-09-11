a5d6a36947fb8a79e52bffae7811d5766643baf2
Tiến TD Laptop <tientd1302@outlook.com>
2021-09-12 01:22:45 +0700

    fix commit

--- Controllers/TestApiController.cs
+++ Controllers/TestApiController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,6 +12,12 @@ namespace bonfml_back.Controllers
     [ApiController]
     public class TestApiController : ControllerBase
     {
+        private readonly ILogger<TestApiController> _logger;
+
+        public TestApiController(ILogger<TestApiController> logger)
+        {
+            _logger = logger;
+        }
         [HttpGet]
         public string Get()
         {
