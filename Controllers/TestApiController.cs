16b87c39b40c92c427218e239d0b6be12410e109
Tiến TD Laptop <tientd1302@outlook.com>
2021-09-12 01:19:02 +0700

    Create test api

---
+++ Controllers/TestApiController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace bonfml_back.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestApiController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "Success!";
+        }
+    }
+}
